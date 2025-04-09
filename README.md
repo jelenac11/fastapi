07a9f8aff50f0db94ce6ff27030629e4d42aca46
Cupac, Jelena <52001925+jelenac11@users.noreply.github.com>
2025-04-09 08:14:37 +0200

    chore: initial project skeleton

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# fastapi
-FastAPI + Pydantic + SqlAlchemy
+# fastapi-sqlalchemy
---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Response, status
+from fastapi.responses import JSONResponse
+
+app = FastAPI(
+    title="FASTAPI+SQLAlchemy+Pydantic",
+)
+
+@app.get("/test")
+def health() -> Response:
+    """Provides a health check of the service.
+    """
+    return JSONResponse(content={"message": "Service is healthy!"}, status_code=status.HTTP_200_OK)
